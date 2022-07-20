dc89955299e19a4c936a78ba099e2d48e27fa852
Kirill Sidelkovsky <sidelkovsky@tuta.io>
2022-07-20 11:20:24 +0400

    initial index template, view test

--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import request
+from django.http import request, HttpResponse
+
 
 def index(request):
-    pass
+    return render(request, 'solos/index.html')
+
