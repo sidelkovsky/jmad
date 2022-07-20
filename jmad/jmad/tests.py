c006255aafa9ce0fc24cd3665bdbca642c237a58
Kirill Sidelkovsky <sidelkovsky@tuta.io>
2022-07-20 11:41:01 +0400

    adds form elements, fake search results to index.html

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -25,11 +25,25 @@ class StudentTestCase(LiveServerTestCase):
         brand_element = self.browser.find_element(By.CLASS_NAME, 'navbar-brand')
         self.assertEqual('JMAD', brand_element.text)
         # He sees the inputs of the search form, including labels and placeholders.
+        instrument_input = self.browser.find_element(By.CSS_SELECTOR, 'input#jmad-instrument')
+        self.assertIsNotNone(self.browser.find_element(By.CSS_SELECTOR, 'label[for="jmad-instrument"]'))
+        self.assertEqual(instrument_input.get_attribute('placeholder'), 'i.e. trumpet')
+        artist_input = self.browser.find_element(By.CSS_SELECTOR, 'input#jmad-artist')
+        self.assertIsNotNone(self.browser.find_element(By.CSS_SELECTOR, 'label[for="jmad-artist"]'))
+        self.assertEqual(artist_input.get_attribute('placeholder'), 'i.e. Davis')
         # He types in the name of his instrument and submits it.
+        instrument_input.send_keys('saxophone')
+        self.browser.find_element(By.CSS_SELECTOR, 'form button').click()
         # He sees too many search results...
+        search_results = self.browser.find_elements(By.CSS_SELECTOR, '.jmad-search-result')
+        self.assertGreater(len(search_results), 2)
         # ...so he adds an artist to his search query and gets a more manageable list.
+        second_artist_input = self.browser.find_element(By.CSS_SELECTOR, 'input#jmad-artist')
+        second_artist_input.send_keys('Cannonball Adderley')
+        self.browser.find_element(By.CSS_SELECTOR, 'form button').click()
+        second_search_results = self.browser.find_elements(By.CSS_SELECTOR, '.jmad-search-result')
+        self.assertEqual(len(search_results), 2)
         # He clicks on a search result.
         # The solo page has the title, artist and album for this particular solo.
         # He also sees the start time and end time of the solo.
-        print(self.live_server_url)
         self.fail("Incomplete test!")
